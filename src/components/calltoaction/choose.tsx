7d74f6198eac20427ab1061dec42ba565e062ad7
kiwihodl <orangeamber@protonmail.com>
2024-01-04 20:22:05 -0600

    speeding up times for typing and deleting in landing page

--- src/components/calltoaction/choose.tsx
+++ src/components/calltoaction/choose.tsx
@@ -1,6 +1,74 @@
 import { Box, Flex, Text, Grid } from "@chakra-ui/react";
 import React, { useState } from "react";
 import ArticleCard from "./articlecard"
+import styled, { css, keyframes } from 'styled-components';
+
+const glowing = keyframes`
+  0% { background-position: 0 0; }
+  50% { background-position: 400% 0; }
+  100% { background-position: 0 0; }
+`;
+
+const GlowOnHover = styled.button`
+  width: 220px;
+  height: 50px;
+  border: none;
+  outline: none;
+  color: #fff;
+  background: #111;
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
+  border-radius: 10px;
+  margin: 21px;
+
+  &:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: ${glowing} 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+  }
+
+  &:active {
+    color: #000
+  }
+
+  &:active:after {
+    background: transparent;
+  }
+
+  &:hover:before {
+    opacity: 1;
+  }
+
+  &:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+  }
+
+  ${props => props.active && css`
+    &:before {
+      opacity: 1;
+    }
+  `}
+`;
 
 const Choose = () => {
     const [selectedOption, setSelectedOption] = useState("learn");
@@ -16,27 +84,18 @@ const Choose = () => {
             px={4}
         >
             <Flex justifyContent="center" alignItems="center" mb={4}>
-                <Text
-                    fontSize="xl"
-                    fontWeight="bold"
-                    color="white"
-                    mr={4}
-                    cursor="pointer"
+                <GlowOnHover
+                    active={selectedOption === "learn"}
                     onClick={() => handleOptionChange("learn")}
-                    textDecoration={selectedOption === "learn" ? "underline" : "none"}
                 >
                     Learn
-                </Text>
-                <Text
-                    fontSize="xl"
-                    fontWeight="bold"
-                    color="white"
-                    cursor="pointer"
+                </GlowOnHover>
+                <GlowOnHover
+                    active={selectedOption === "hire"}
                     onClick={() => handleOptionChange("hire")}
-                    textDecoration={selectedOption === "hire" ? "underline" : "none"}
                 >
                     Hire
-                </Text>
+                </GlowOnHover>
             </Flex>
             {selectedOption === "learn" && (
                 <Box>
--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -19,15 +19,37 @@ const rotate = keyframes`
   }
 `;
 
+const toRotate = ["Developer", "Voluntarist", "Bitcoiner"];
+
 const Landing: React.FC = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const toRotate = ["Developer", "Voluntarist", "Bitcoiner"];
   const period = 2000;
 
   useEffect(() => {
+    const tick = () => {
+      let i = loopNum % toRotate.length;
+      let fullText = toRotate[i];
+      let updatedText = isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1);
+
+      setText(updatedText);
+
+      if (!isDeleting && text === fullText) {
+        setDelta(2100);
+        setIsDeleting(true);
+      } else if (isDeleting && text === '') {
+        setIsDeleting(false);
+        setLoopNum(loopNum + 1);
+        setDelta(500 - Math.random() * 100);
+      } else {
+        setDelta(isDeleting ? 30 : 60);
+      }
+    };
+
     let ticker = setInterval(() => {
       tick();
     }, delta);
@@ -35,30 +57,7 @@ const Landing: React.FC = () => {
     return () => {
       clearInterval(ticker);
     };
-  }, [text, delta]);
-
-  const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting
-      ? fullText.substring(0, text.length - 1)
-      : fullText.substring(0, text.length + 1);
-
-    setText(updatedText);
-
-    if (isDeleting) {
-      setDelta(delta / 2);
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setDelta(period);
-    } else if (isDeleting && updatedText === "") {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setDelta(500);
-    }
-  };
+  }, [text, isDeleting, loopNum, delta]);
 
   return (
     <Grid
