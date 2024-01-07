5872137ff6a92aab6800ffb1b213ee04de155f8c
kiwihodl <orangeamber@protonmail.com>
2024-01-07 15:50:40 -0600

    adding side bar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import theme from "./utils/theme.ts";
 import Nav from "./components/navbar/navbar.tsx"
 import Landing from './components/landing/landing.tsx';
 import Choose from './components/calltoaction/choose.tsx'
+import Sidebar from './components/sidebar/sideBar.tsx'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Nav />
+      <Sidebar />
       <Landing />
       <Choose />
     </ChakraProvider>
---
+++ src/components/sidebar/sideBar.tsx
@@ -0,0 +1,61 @@
+import { Box, Link, Icon, VStack, useMediaQuery } from "@chakra-ui/react";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import React from "react";
+import { keyframes } from '@emotion/react';
+
+const glow = keyframes`
+  0% {
+    filter: drop-shadow(0 0 5px #FFA500) drop-shadow(0 0 10px #FFA500) drop-shadow(0 0 15px #FFA500) drop-shadow(0 0 20px #FFA500);
+  }
+  100% {
+    filter: drop-shadow(0 0 10px #FFA500) drop-shadow(0 0 20px #FFA500) drop-shadow(0 0 30px #FFA500) drop-shadow(0 0 40px #FFA500);
+  }
+`;
+
+const Sidebar: React.FC = () => {
+  const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
+
+  return (
+    <Box
+      position="fixed"
+      left="0"
+      bottom="0"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="flex-end"
+      alignItems="center"
+      paddingLeft="20px"
+    >
+      <VStack spacing={2} align="center" marginBottom="5px">
+        <Link href="https://www.facebook.com" isExternal>
+          <Icon as={FaFacebook} color="#FFA500" boxSize="24px" css={{
+            transition: "all 0.2s ease-in-out",
+            "&:hover": {
+              animation: `${glow} 1s ease-in-out infinite alternate`,
+            },
+          }} />
+        </Link>
+        <Link href="https://www.twitter.com" isExternal>
+          <Icon as={FaTwitter} color="#FFA500" boxSize="24px" css={{
+            transition: "all 0.2s ease-in-out",
+            "&:hover": {
+              animation: `${glow} 1s ease-in-out infinite alternate`,
+            },
+          }} />
+        </Link>
+        <Link href="https://www.instagram.com" isExternal>
+          <Icon as={FaInstagram} color="#FFA500" boxSize="24px" css={{
+            transition: "all 0.2s ease-in-out",
+            "&:hover": {
+              animation: `${glow} 1s ease-in-out infinite alternate`,
+            },
+          }} />
+        </Link>
+      </VStack>
+      <Box borderLeft="1px solid #FFA500" height="121px" alignSelf="center" />
+    </Box>
+  );
+};
+
+export default Sidebar;
